fb26577ea21988ee5f12ac3acd837c664ad36874
zarghammughal1 <55196035+zarghammughal1@users.noreply.github.com>
2022-10-26 22:26:04 +0500

    Upadte code and add security

--- Email_Sender.py
+++ Email_Sender.py
@@ -5,14 +5,14 @@ from string import Template
 
 html = Template(Path('html_file.html').read_text())
 email = EmailMessage()
-email['from'] = 'Zeeshan Asim'
-email['to'] = 'zeeshanravian1@gmail.com'
+email['from'] = 'Sender Name'
+email['to'] = 'Receiver Email'
 email['subject'] = 'Job Vacency'
 email.set_content('hjftyc')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('honeybutt1995@gmail.com', '123456789@H')
+    smtp.login('Sender Email', 'Sender Email Password')
     smtp.send_message(email)
     print('Done!')
